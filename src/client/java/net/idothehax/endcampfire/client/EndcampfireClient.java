4e9bd4404783e98842915a424482a938b627c24c
IDoTheHax <hax@idothehax.com>
2025-04-18 18:39:00 +0200

    Init Project + gradle wrapper

---
+++ src/client/java/net/idothehax/endcampfire/client/EndcampfireClient.java
@@ -0,0 +1,10 @@
+package net.idothehax.endcampfire.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EndcampfireClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/idothehax/endcampfire/Endcampfire.java
@@ -0,0 +1,10 @@
+package net.idothehax.endcampfire;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Endcampfire implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
